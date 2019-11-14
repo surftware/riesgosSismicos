9b67f0e36f2e634edece846c2877394494d6dc38
and <surftware@gmail.com>
2019-11-14 17:33:40 -0600

    Inicialización creación base de datos

---
+++ db/riesgosSismicos.sql
@@ -0,0 +1,58 @@
+create table tipoVialidad(
+    idVialidad integer not null AUTO_INCREMENT,
+    tipoVialidad char(25) not null,
+
+    PRIMARY KEY(idVialidad)    
+    );
+LOAD DATA LOCAL INFILE '/var/www/riesgosSismicos/db/tipoVialidad.csv' INTO TABLE tipoVialidad CHARACTER SET UTF8 FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n';
+
+
+create table daño(
+    idDaño integer not null AUTO_INCREMENT,
+    tipoDaño char(35),
+    
+    PRIMARY KEY(idDaño)
+);
+
+
+create table Ubicacion(
+
+    idUbicacion integer not null AUTO_INCREMENT,
+    nombreVialidad char(25),
+    numeroExterior char(10),
+    idTipoVialidad integer not null,
+    
+    PRIMARY KEY(idUbicacion),
+    FOREIGN KEY(idTipoVialidad) REFERENCES tipoVialidad(idVialidad)
+
+
+);
+
+
+create table Edificio(
+    idEdificio integer not null AUTO_INCREMENT,
+    nombreEdificio char(25),
+    usoSuelo char(30) not null,
+    ctaCatastral char(20),
+    idUbicacionEdificio integer not null,
+
+    PRIMARY KEY(idEdificio),
+    FOREIGN KEY(idUbicacionEdificio) REFERENCES Ubicacion(idUbicacion)
+
+);
+
+
+
+create table edificioSufreDaño(
+    idEdificioSufreDaño integer not null AUTO_INCREMENT,
+    Edificio integer not null,
+    Daño integer not null,
+
+    PRIMARY KEY(idEdificioSufreDaño),
+    FOREIGN KEY(Edificio) REFERENCES Edificio(idEdificio),
+    FOREIGN KEY(Daño) REFERENCES daño(idDaño)
+);
+
+
+
+
