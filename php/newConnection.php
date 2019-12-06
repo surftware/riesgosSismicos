6ba07aa6622b6545f1bec8a1374a11697a299e3c
CeciliaGoJi <cecygomjim20@comunidad.unam.mx>
2019-12-06 16:31:10 -0600

    Conexion PHP modificada

--- php/newConnection.php
+++ php/newConnection.php
@@ -1,15 +1,14 @@
 <?php
-    $servidor = "localhost";      
+    $servidor = "localhost";
     $usuario = "surftware";
-    $contrasena = "#Fr1d1ta#";  // en mi caso tengo contraseña pero en casa caso introducidla aquí.   
-    $basededatos = "agenda";
+    $contrasena = "#Fr1d1ta#";  // en mi caso tengo contraseña pero en casa caso introducidla aquí.
+    $basededatos = "riesgosSismicos";
 
     $buscar= $_POST['consultar'];
-  
+
 
     $conexion = mysqli_connect($servidor,$usuario,$contrasena) or die ("No se ha podido conectar al servidor de Base de datos");
     $db = mysqli_select_db($conexion,$basededatos )or die ("No existe la base de datos");
 
-    
-    ?>
 
+    
